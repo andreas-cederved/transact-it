e22e44dba9369bccd4cef1759b027f18643f8ad5
Andreas Cederved <andreas.cederved@faultier.se>
2019-07-04 18:03:33 +0200

    Added create ledgerAccount feature

---
+++ src/TransactIt.Application.Write/LedgerAccounts/SaveLedgerAccountValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace TransactIt.Application.Write.LedgerAccounts
+{
+    public class SaveLedgerAccountValidator : AbstractValidator<SaveLedgerAccountRequest>
+    {
+        public SaveLedgerAccountValidator()
+        {
+            RuleFor(x => x.LedgerAccount).NotNull().DependentRules(() =>
+            {
+                RuleFor(x => x.LedgerAccount.Number).GreaterThan(0);
+                RuleFor(x => x.LedgerAccount.Name).NotEmpty();
+            });
+
+            RuleFor(x => x.LedgerSubAccountGroupId).GreaterThan(0);
+        }
+    }
+}
